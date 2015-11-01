88a6efc03a0bd8930ecece520043224a7da561de
Jeff Hoogland <jeffhoogland@linux.com>
2015-11-01 15:35:08 -0600

    Moved StandardButton and StandardPopup to be proper classes to inherit properties

--- elmextensions/StandardButton.py
+++ elmextensions/StandardButton.py
@@ -8,14 +8,13 @@ EXPAND_BOTH = EVAS_HINT_EXPAND, EVAS_HINT_EXPAND
 EXPAND_HORIZ = EVAS_HINT_EXPAND, 0.0
 FILL_BOTH = EVAS_HINT_FILL, EVAS_HINT_FILL
 
-def StandardButton(ourParent, ourText, ourIcon=None, ourCB=None, *args, **kwargs):
-    icon = Icon(ourParent, size_hint_weight=EXPAND_BOTH, size_hint_align=FILL_BOTH)
-    icon.standard_set(ourIcon)
-    icon.show()
+class StandardButton(Button):
+    def __init__(self, ourParent, ourText, ourIcon=None, ourCB=None, *args, **kwargs):
+        Button.__init__(self, ourParent, *args, **kwargs)
+        icon = Icon(self, size_hint_weight=EXPAND_BOTH, size_hint_align=FILL_BOTH)
+        icon.standard_set(ourIcon)
+        icon.show()
         
-    button = Button(ourParent, *args, **kwargs)
-    button.text = ourText
-    button.content_set(icon)
-    button.callback_clicked_add(ourCB)
-    
-    return button
+        self.text = ourText
+        self.content_set(icon)
+        self.callback_clicked_add(ourCB)
--- elmextensions/StandardPopup.py
+++ elmextensions/StandardPopup.py
@@ -17,51 +17,50 @@ FILL_BOTH = EVAS_HINT_FILL, EVAS_HINT_FILL
 FILL_HORIZ = EVAS_HINT_FILL, 0.5
 ALIGN_CENTER = 0.5, 0.5
 
-def StandardPopup(ourParent, ourMsg, ourIcon=None, *args, **kwargs):
-    ourPopup = Popup(ourParent, *args, **kwargs)
-    ourPopup.callback_block_clicked_add(lambda obj: ourPopup.delete())
+class StandardPopup(Popup):
+    def __init__(self, ourParent, ourMsg, ourIcon=None, *args, **kwargs):
+        Popup.__init__(self, ourParent, *args, **kwargs)
+        self.callback_block_clicked_add(lambda obj: self.delete())
 
-    # Add a table to hold dialog image and text to Popup
-    tb = Table(ourPopup, size_hint_weight=EXPAND_BOTH)
-    ourPopup.part_content_set("default", tb)
-    tb.show()
+        # Add a table to hold dialog image and text to Popup
+        tb = Table(self, size_hint_weight=EXPAND_BOTH)
+        self.part_content_set("default", tb)
+        tb.show()
 
-    # Add dialog-error Image to table
-    need_ethumb()
-    icon = Icon(ourPopup, thumb='True')
-    icon.standard_set(ourIcon)
-    # Using gksudo or sudo fails to load Image here
-    #   unless options specify using preserving their existing environment.
-    #   may also fail to load other icons but does not raise an exception
-    #   in that situation.
-    # Works fine using eSudo as a gksudo alternative,
-    #   other alternatives not tested
-    try:
-        dialogImage = Image(ourPopup,
+        # Add dialog-error Image to table
+        need_ethumb()
+        icon = Icon(self, thumb='True')
+        icon.standard_set(ourIcon)
+        # Using gksudo or sudo fails to load Image here
+        #   unless options specify using preserving their existing environment.
+        #   may also fail to load other icons but does not raise an exception
+        #   in that situation.
+        # Works fine using eSudo as a gksudo alternative,
+        #   other alternatives not tested
+        try:
+            dialogImage = Image(self,
+                                size_hint_weight=EXPAND_HORIZ,
+                                size_hint_align=FILL_BOTH,
+                                file=icon.file_get())
+            tb.pack(dialogImage, 0, 0, 1, 1)
+            dialogImage.show()
+        except RuntimeError:
+            # An error message is displayed for this same error
+            #   when aboutWin is initialized so no need to redisplay.
+            pass
+        # Add dialog text to table
+        dialogLabel = Label(self, line_wrap=ELM_WRAP_WORD,
                             size_hint_weight=EXPAND_HORIZ,
-                            size_hint_align=FILL_BOTH,
-                            file=icon.file_get())
-        tb.pack(dialogImage, 0, 0, 1, 1)
-        dialogImage.show()
-    except RuntimeError:
-        # An error message is displayed for this same error
-        #   when aboutWin is initialized so no need to redisplay.
-        pass
-    # Add dialog text to table
-    dialogLabel = Label(ourPopup, line_wrap=ELM_WRAP_WORD,
-                        size_hint_weight=EXPAND_HORIZ,
-                        size_hint_align=FILL_BOTH)
-    dialogLabel.text = ourMsg
-    tb.pack(dialogLabel, 1, 0, 1, 1)
-    dialogLabel.show()
+                            size_hint_align=FILL_BOTH)
+        dialogLabel.text = ourMsg
+        tb.pack(dialogLabel, 1, 0, 1, 1)
+        dialogLabel.show()
 
-    # Ok Button
-    ok_btt = Button(ourPopup)
-    ok_btt.text = "Ok"
-    ok_btt.callback_clicked_add(lambda obj: ourPopup.delete())
-    ok_btt.show()
+        # Ok Button
+        ok_btt = Button(self)
+        ok_btt.text = "Ok"
+        ok_btt.callback_clicked_add(lambda obj: self.delete())
+        ok_btt.show()
 
-    # add button to popup
-    ourPopup.part_content_set("button3", ok_btt)
-    
-    return ourPopup
+        # add button to popup
+        self.part_content_set("button3", ok_btt)
