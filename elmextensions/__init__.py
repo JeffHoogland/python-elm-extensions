89fe38163426efe02da92974bac369538ab5532f
Robert Wiley <ylee@bodhilinux.com>
2017-08-01 18:15:40 -0400

    Access to module level information

--- elmextensions/__init__.py
+++ elmextensions/__init__.py
@@ -2,7 +2,15 @@ from .sortedlist import *
 from .embeddedterminal import *
 from .aboutwindow import *
 from .fileselector import *
+from .fontselector import *
 from .tabbedbox import *
 from .StandardButton import *
 from .StandardPopup import *
 from .SearchableList import *
+
+__copyright__  = "Copyright 2015-2017 Jeff Hoogland"
+__license__    = "BSD-3-clause"
+
+# the version number: major, minor, micro, releaselevel, and serial.
+__version__    = "0.2.1rc.2"
+version_string = __version__
